4cf9b3e7c9a749f053fb72c217ce4fc078450891
ItsYasiru <mwmykd@gmail.com>
2022-01-27 13:01:42 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="better-threads",
-    version="0.2.3",
+    version="0.3.0",
     description="The package provides extended control over threads",
     long_description=README,
     long_description_content_type="text/markdown",
